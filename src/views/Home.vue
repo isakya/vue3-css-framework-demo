6bab194eb519012e2c620ae2f71bd4ea26dacae5
konglong <1016361268@qq.com>
2022-07-27 23:59:28 +0800

    tools层代码实现

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <header>CSSjiagou</header>
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
@@ -7,12 +8,21 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from "@/components/HelloWorld.vue"
 
 export default {
   name: "Home",
   components: {
     HelloWorld,
   },
-};
+}
 </script>
+<style lang="scss" scoped>
+header {
+  width: 200px;
+  height: 200px;
+  background: rgba($color: #000000, $alpha: .4);
+  color: #fff;
+  @include box-center(center, center);
+}
+</style>
